29c2e7aa2e458a078fb2476f9a05e0223f897287
Yash <yashgoel9818@gmail.com>
2023-11-24 15:40:37 +0530

    Initial Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import reactRouterDom from "react-dom";
+import "./index.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component{
+  render(){
+    return (
+      <div className="github-profile">
+        <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card/>
+      </div>
+    );
+  }
+}
+
+export default App
