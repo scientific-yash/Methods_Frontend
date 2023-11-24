0e887bed0249569eab2a3ae905af3f0320117f8e
Yash <yashgoel9818@gmail.com>
2023-11-24 15:42:00 +0530

    Update Card

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import React from "react";
-import reactRouterDom from "react-dom";
-import "./index.css";
-import Axios from "axios";
+//import ReactDOM from "react-dom";
+import "./App.css";
+//import Axios from "axios";
 
-class Card extends React.Component{
-  render(){
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+const CardList = (props) => (
+  <div>
+    {testData.map(profile => <Card{...profile} />)}
+  </div>
+);
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" />
+        <img src={profile.avatar_url} alt="p"/>
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
   }
 }
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card/>
+        <CardList />
       </div>
     );
   }
 }
 
-export default App
+export default App;
